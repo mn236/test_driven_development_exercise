0638b5640f0c71260008af07ba8a1873c3d54787
Maryam Nuru <maryamnuru@Maryams-MacBook-Pro.local>
2021-09-14 21:21:21 -0400

    made a change to the test case because of problem with formatting

--- test_point_on_slope.py
+++ test_point_on_slope.py
@@ -2,8 +2,8 @@ import pytest
 
 
 @pytest.mark.parametrize("x1, y1, x2, y2, input_x, expected", [
-    (2, 1, 8, 2, 6, 1.67),
-    (2, 1, 8, 2, 12, 2.67),
+    (2, 1, 8, 2, 6, 1.6666666666666667),
+    (2, 1, 8, 2, 12, 2.666666666666667),
 ])
 def test_calculate_y(x1, y1, x2, y2, input_x, expected):
     from point_on_slope import calculate_y
