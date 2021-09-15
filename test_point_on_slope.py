3b55c585628499766f5d41a14f76ac056623cc61
Maryam Nuru <maryamnuru@Maryams-MacBook-Pro.local>
2021-09-14 20:06:47 -0400

    pep8 compliant formatting

--- test_point_on_slope.py
+++ test_point_on_slope.py
@@ -1,10 +1,11 @@
 import pytest
 
+
 @pytest.mark.parametrize("input_x, expected", [
-    (6, 1.67 ),
+    (6, 1.67),
     (12, 2.67),
     (3, 1.17)
-] )
+])
 def test_calculate_y(input_x, expected):
     from point_on_slope import calculate_y
     answer = calculate_y(input_x)
